015d564c29abaa9b3760ed3e9500a9d25c1397b4
kazusar123 <kazusar123@gmail.com>
2025-06-12 01:21:43 +0900

    画像パスの変換テスト

--- next-app/export-images.config.js
+++ next-app/export-images.config.js
@@ -1,10 +1,9 @@
 /**
  * @type {import('next-export-optimize-images').Config}
  */
-// const isGitHubPages = process.env.NEXT_PUBLIC_DEPLOY_TARGET === "github";
+const isGitHubPages = process.env.NEXT_PUBLIC_DEPLOY_TARGET === "github";
 const config = {
-  // basePath: isGitHubPages ? "/kinokotogame.club" : "",
-  // basePath: "/kinokotogame.club",
+  basePath: isGitHubPages ? "/kinokotogame.club" : "",
 };
 
 export default config;
