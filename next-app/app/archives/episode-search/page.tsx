a9b8f064f34419057c11249b15a21f441c3971f0
kazusar123 <kazusar123@gmail.com>
2025-08-19 00:31:55 +0900

    エピソード検索のページの最終更新日を変更。

--- next-app/app/archives/episode-search/page.tsx
+++ next-app/app/archives/episode-search/page.tsx
@@ -19,7 +19,7 @@ export default function Page() {
           <p className="leading-7">
             ・ガールを選択して検索ボタンをタップすると、そのガールが登場するエピソードをデータベースから検索して表示します。
             <br />
-            ・最終更新日: 2025年04月26日
+            ・最終更新日: 2025年08月19日
           </p>
         </section>
         <EpisodeSearch />
