068e08bd6de13cc5309551d2a1c86e397b155a81
kazusar <kazusar123@gmail.com>
2026-03-25 22:07:12 +0900

    プレシャスシーンに[全力応援！]白水六花を追加。

--- next-app/features/decksim/data/preciousScenesData.ts
+++ next-app/features/decksim/data/preciousScenesData.ts
@@ -542,6 +542,13 @@ export const PRECIOUS_SCENES_DATA: { [K: number]: PreciousScenes } = {
     value1: 6.0, value2: 6.0, value3: 6.0, value4: 7.0, value5: 8.0,
     factor: 1.5,
   },
+  112: {
+    name: "[全力応援！]白水六花", rarity: 3, valueFormat: "割合(%)",
+    effectTarget: "SWEETタイプ", effectRange: "主＋副", effectType: "攻援UP",
+    effectCondition: "様々なガールで編成するほど", conditionThreshold: 25,
+    value1: 6.0, value2: 6.0, value3: 6.0, value4: 7.0, value5: 8.0,
+    factor: 1.5,
+  },
 
   108: {
     name: "[13周年]メモリアル", rarity: 3, valueFormat: "割合(%)",
