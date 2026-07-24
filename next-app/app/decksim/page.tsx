1a4479fe892937856d94a3631d992d94af993d8f
kazusar <kazusar123@gmail.com>
2026-07-24 22:20:36 +0900

    プレシャスシーンに[お仕事ﾓｰﾄﾞ]見吉奈央を暫定値で追加。

--- next-app/app/decksim/page.tsx
+++ next-app/app/decksim/page.tsx
@@ -254,6 +254,16 @@ export default function Page() {
                     → いくつかの人数での実際の表示効果値。★4の時の最大効果値。
                   </span>
                 </div>
+                <div className="mt-2 md:pl-4">
+                  <p className="pl-4 relative before:w-2 before:h-2 before:rounded-full before:bg-accent before:inline-block before:absolute before:left-0 before:top-[12px]">
+                    <span>
+                      [お仕事ﾓｰﾄﾞ]見吉奈央 (COOLガールの声援Lvが高いほど攻守UP)
+                    </span>
+                  </p>
+                  <span className="pl-4">
+                    → いくつかの人数での実際の表示効果値。★4の時の最大効果値。
+                  </span>
+                </div>
               </div>
 
               <div>
--- next-app/features/decksim/data/preciousScenesData.ts
+++ next-app/features/decksim/data/preciousScenesData.ts
@@ -595,6 +595,29 @@ export const PRECIOUS_SCENES_DATA: { [K: number]: PreciousScenes } = {
     annotation: "減衰係数に推定値を使用しています。情報募集中です。",
   },
 
+  114: {
+    name: "[お仕事ﾓｰﾄﾞ]見吉奈央", rarity: 3, valueFormat: "割合(%)",
+    effectTarget: "COOLタイプ", effectRange: "主＋副", effectType: "攻守UP",
+    effectCondition: "声援Lvが高いほど", conditionThreshold: 17,
+    value1: 6.0, value2: 6.0, value3: 6.0, value4: 7.0, value5: 8.0,
+    factor: 1.05,
+    annotation: "減衰係数に推定値を使用しています。情報募集中です。",
+  },
+  // 97: {
+  //   name: "[よそ見注意]朝比奈桃子", rarity: 3, valueFormat: "割合(%)",
+  //   effectTarget: "SWEETタイプ", effectRange: "主＋副", effectType: "攻守UP",
+  //   effectCondition: "声援Lvが高いほど", conditionThreshold: 17,
+  //   value1: 6.0, value2: 6.0, value3: 6.0, value4: 7.0, value5: 8.0,
+  //   factor: 1.05,
+  // },
+  // 98: {
+  //   name: "[きみが一番]相楽エミ", rarity: 3, valueFormat: "割合(%)",
+  //   effectTarget: "POPタイプ", effectRange: "主＋副", effectType: "攻守UP",
+  //   effectCondition: "声援Lvが高いほど", conditionThreshold: 17,
+  //   value1: 6.0, value2: 6.0, value3: 6.0, value4: 7.0, value5: 8.0,
+  //   factor: 1.05,
+  // },
+
   /* 初期星2 */
   7: {
     name: "[緊張ﾁﾗｼ配ﾘ]白水六花", rarity: 2, valueFormat: "割合(%)",
